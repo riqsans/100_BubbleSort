e36f3fb1afe0b6dfc97ee8def5e2d5e3d4725359
riqsans <thrq88@gmail.com>
2024-03-08 15:32:09 +0700

    Prosedur untuk input

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -7,5 +7,24 @@ using namespace std;
 
 int arr[20]; // Deklarasi variabel global array a dengan ukuran 20
 int n; // Deklarasi variabel global n unutuk menyimpan banyaknya elemen array
+void inout() {  //procedur untuk input
+
+	while (true) {
+		cout << "Masukan banyaknya elemen pada array : "; //outuput ke layar
+		cin >> n; //input dari pengguna
+		if (n <= 20) //jika n kurang dari atau sama dengan 20
+			break; // keluar dari loop
+		else { //jika n lebih dari 20
+			cout << "\nArray dapat mempunyai aksimal 20 elemen.\n" // output ke layar
+		}
+	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "====================" << endl;
+
+	
+}
+
 
 
