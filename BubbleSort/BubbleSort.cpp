f79fc2a33e77e012bc216dd576e95f683e55ee1a
riqsans <thrq88@gmail.com>
2024-03-08 15:30:19 +0700

    Revert "100_20230140100"
    This reverts commit 011569dbb16d15bc298d93ae210bd2001eebd8cb.

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int arr[20]; // Deklarasi variabel global array a dengan ukuran 20
-
 int n; // Deklarasi variabel global n unutuk menyimpan banyaknya elemen array
-
