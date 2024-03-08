011569dbb16d15bc298d93ae210bd2001eebd8cb
riqsans <thrq88@gmail.com>
2024-03-08 15:27:28 +0700

    100_20230140100

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
 int arr[20]; // Deklarasi variabel global array a dengan ukuran 20
+
 int n; // Deklarasi variabel global n unutuk menyimpan banyaknya elemen array
+
