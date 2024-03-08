8d21ba214c9956e10c58dabb2dd97f78376c3909
riqsans <thrq88@gmail.com>
2024-03-08 16:19:35 +0700

    menambahkan prosedur utana

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int arr[20]; // Deklarasi variabel global array a dengan ukuran 20
 int n; // Deklarasi variabel global n unutuk menyimpan banyaknya elemen array
-void inout() {  //procedur untuk input
+void input() {  //procedur untuk input
 
 	while (true) {
 		cout << "Masukan banyaknya elemen pada array : "; //outuput ke layar
@@ -16,13 +16,12 @@ void inout() {  //procedur untuk input
 			break; // keluar dari loop
 		else { //jika n lebih dari 20
 			cout << "\nArray dapat mempunyai aksimal 20 elemen.\n"; // output ke layar
+			cout << endl;
+			cout << "====================" << endl;
+			cout << "Masukan Elemen Array" << endl;
+			cout << "====================" << endl;
 		}
 	}
-	cout << endl;
-	cout << "====================" << endl;
-	cout << "Masukan Elemen Array" << endl;
-	cout << "====================" << endl;
-
 	for (int i = 0; i < n; i++) {  //looping dengan i dimulai dari 0 hingga n-1
 		cout << "Data ke-" << (i + 1) << ":"; //output ke layar
 		cin >> arr[i];
@@ -72,4 +71,12 @@ void display() {
 	cout << endl;
 	cout << endl;
 
+}
+int main() {
+	input();
+	bubbleSortArray();
+	display();
+
+	system("pause");
+	return 0;
 }
